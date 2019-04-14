c011ce357c9eab15308cd3a0b5c68ff825ea1f7b
Luxelbot <49617463+Luxelbot@users.noreply.github.com>
2019-04-14 21:56:17 +0300

    Update botmesaj.js

--- komutlar/botmesaj.js
+++ komutlar/botmesaj.js
@@ -10,7 +10,7 @@ module.exports.run = async (client, msg, args) => {
         let embed = new Discord.RichEmbed()
         .setTitle("Luxel Özel Mesaj!")
         .setColor("RANDOM")
-        .addField(mesaj, true)
+        .addField("Mesaj İçeriği", mesaj, true)
         .setFooter(client.user.username, client.user.avatarURL)
         .setTimestamp();
         uyeler.user.send(embed);
