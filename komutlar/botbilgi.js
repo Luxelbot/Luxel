0323c3e15d60e8ab2463bc288aa358ce7dad7284
Luxelbot <49617463+Luxelbot@users.noreply.github.com>
2019-04-14 19:25:45 +0300

    Update botbilgi.js

--- komutlar/botbilgi.js
+++ komutlar/botbilgi.js
@@ -1,1 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, msg, args) => {
+    let embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setTitle(client.user.username + " Bot Bilgi")
+    .addField("Hizmet verilen sunucu sayısı", client.guilds.size, true)
+    .addField("Discord JS Sürümü", "v"+Discord.version, true)
+    .addField("Ping", client.ping + " MS",true)
+    .setFooter(client.user.username + " v1.0", client.user.avatarURL);
+
+    msg.channel.send(embed);
+}
+
+module.exports.help = {
+    name: "botbilgi"
+}
 
