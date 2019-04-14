a78be23d5ab94d62907a8a89b549577ce22ff333
Luxelbot <49617463+Luxelbot@users.noreply.github.com>
2019-04-14 19:24:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,24 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const ayar = require("./ayar.json");
+const fs = require("fs");
+client.commands = new Discord.Collection();
+
+    fs.readdir("./komutlar/", (err, files) => {
+
+        if(err) console.log(err);
+
+        let jsfile = files.filter(f => f.split(".").pop() === "js")
+        if(jsfile.length <= 0){
+            return console.log("Komut Bulunamadı!");
+        }
+
+        jsfile.forEach((f, i) => {
+            let props = require(`./komutlar/${f}`);
+            console.log(`[${i + 1}] - ${f} Yüklendi!`);
+            client.commands.set(props.help.name, props);
+        });
+    });
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -9,10 +27,14 @@ client.on("ready", () => {
 let prefix = ayar.prefix;
 
 client.on("message", msg => {
+  
+    let prefix = ayar.prefix;
+    let messageArray = msg.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
 
-    if (msg.content == prefix + "ping"){
-        msg.reply(client.ping);
-    }
+    let commandfile = client.commands.get(cmd.slice(prefix.length));
+    if(commandfile) commandfile.run(client,msg,args);
 
 });
 
