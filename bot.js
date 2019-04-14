08a5e2244c8dd32e2b4225a7cf228f9d7bd6cf14
Luxelbot <49617463+Luxelbot@users.noreply.github.com>
2019-04-14 20:16:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,37 +38,4 @@ client.on("message", msg => {
 
 });
 
-client.login(process.env.TOKEN); // prefix.token
-
-
-client.on('guildMemberAdd', msg => { // Commands Go Inside The client.on('message', 
-msg => )
-msg.guild.channels.get('549584589107232791').send({embed: {
-color: 3447003,
-author: {
-  name: client.user.username,
-  icon_url: client.user.avatarURL
-},
-title: "Welcome To ()!",
-url: "https://districtservices.net",
-description: "@MEMBER",
-fields: [{
-    name: "Fields",
-    value: "They can have different fields with small headlines."
-  },
-  {
-    name: "Masked links",
-    value: "You can put [masked links](http://google.com) inside of rich embeds."
-  },
-  {
-    name: "Markdown",
-    value: "You can put all the *usual* **__Markdown__** inside of them."
-  }
-],
-timestamp: new Date(),
-footer: {
-  icon_url: client.user.avatarURL,
-  text: "© Example"
-}
-
 client.login(process.env.TOKEN);
