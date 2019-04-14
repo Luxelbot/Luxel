597786d7e875d04dd94507675c5339e8fccc451e
Luxelbot <49617463+Luxelbot@users.noreply.github.com>
2019-04-14 22:14:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ client.on("ready", () => { //
 });
 
 client.on("message", (message) => {
-    if (message.content.startsWith("/kick")) {
+    if (message.content.startsWith("l!kick")) {
         // Easy way to get member object though mentions.
         var member= message.mentions.members.first();
         // Kick
