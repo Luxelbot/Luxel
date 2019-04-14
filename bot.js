4ad863a30f489cafa9ca4d18c3b32f717986890c
Luxelbot <49617463+Luxelbot@users.noreply.github.com>
2019-04-14 20:37:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,13 +20,13 @@ client.commands = new Discord.Collection();
         });
     });
 
-client.on("guildMemberAdd" ,(msg, member) => {
+client.on("guildMemberAdd" ,member => {
     let kanal = member.guild.channels.find(ch => ch.name === "gelen");
     if(!kanal) return console.log("gelen adında kanal bulunamadı"); // kanal yok ise
     kanal.send(member.user.tag + member.guild.name +" Sunucumuza hoşgeldin!");
 });
 
-client.on("guildMemberRemove" ,(msg, member) => {
+client.on("guildMemberRemove" ,member => {
     let kanal = member.guild.channels.find(ch => ch.name === "giden"); // kanal arama
     if(!kanal) return console.log("giden adında kanal bulunamadı"); // kanal yok ise
     kanal.send(member.user.tag + " Sunucumuzdan çıkış yaptı!");
