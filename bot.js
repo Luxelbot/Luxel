9eca9bd30cdef906899046d892c5b344e191b8f5
Luxelbot <49617463+Luxelbot@users.noreply.github.com>
2019-04-14 22:07:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,6 +55,21 @@ client.on("guildMemberRemove" ,member => {
 client.on("ready", () => { // 
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.on("message", (message) => {
+    if (message.content.startsWith("l!at")) {
+        // Easy way to get member object though mentions.
+        var member= message.mentions.members.first();
+        // Kick
+        member.kick().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " Başarılı Şekilde Atıldı! :point_right: ");
+        }).catch(() => {
+             // Failmessage
+            message.channel.send("Yetkiniz Yok!");
+        });
+    }
+});
   
 let prefix = ayar.prefix;
 
