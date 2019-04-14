f54a658d90d2cb5fd1a5437c4dbcaa8851ca6135
Luxelbot <49617463+Luxelbot@users.noreply.github.com>
2019-04-14 19:39:22 +0300

    Update botbilgi.js

--- komutlar/botbilgi.js
+++ komutlar/botbilgi.js
@@ -4,7 +4,6 @@ module.exports.run = async (client, msg, args) => {
     let embed = new Discord.RichEmbed()
     .setColor("RANDOM")
     .setAuthor(client.user.username + " Bot Bilgi",client.user.avatarURL)
-    .setTitle(client.user.username + " Bot Bilgi")
     .addField("Sunucu Sayısı", client.guilds.size)
     .addField("Discord JS Sürümü", "v"+Discord.version, true)
     .addField("Ping", client.ping + " MS",true)
