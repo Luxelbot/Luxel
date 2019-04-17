3beeb3dca97c454fae80223ae30fb87da47d5c36
Luxelbot <49617463+Luxelbot@users.noreply.github.com>
2019-04-17 22:42:57 +0300

    Update botmesaj.js

--- komutlar/botmesaj.js
+++ komutlar/botmesaj.js
@@ -4,6 +4,11 @@ module.exports.run = async (client, msg, args) => {
 
     let mesaj = args.join(" ");
     if(mesaj == null) return msg.channel.send("Bir mesaj giriniz.");
+    }
+    else {
+         if (!messenger.member.hasPermission("MANAGE_MESSAGES")){
+             return messenger.reply("Bu komutu kullanmak için gerekli izine sahip değilsin!");
+
     
 
     msg.guild.members.forEach(function(uyeler) {
@@ -18,9 +23,8 @@ module.exports.run = async (client, msg, args) => {
     });
 }
 
-let mesaj = args.join(" ");
-if (!messenger.member.hasPermission("MANAGE_MESSAGES")){
-        return messenger.reply("Bu komutu kullanmak için gerekli izine sahip değilsin!");
+
+
 }
 
 module.exports.help = {
