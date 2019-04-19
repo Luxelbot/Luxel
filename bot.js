e4541beff8c44e1d53be46cba7d9318fb147006c
Luxelbot <49617463+Luxelbot@users.noreply.github.com>
2019-04-19 12:52:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,36 +63,6 @@ client.on('message', msg => {
   }
 });
 
-
-client.on('message', msg => {
-  if (msg.content === 'l!yetkili') {
-    msg.reply('```l!temizle Yazılan Mesajları Siler.```
-              ```l!unban Banlanan Kişilerin Banını Kaldırır.```');
-  }
-    
-client.on('message', msg => {
-  if (msg.content === 'l!yardim') {
-    msg.reply('```l!kullanıcı Kullanıcı Komutlar```
-              ```l!yetkili Yetkili Komutlar```
-              ```l!eğlence Eğlence Komutlar```');
-  }
-});
-
-
-client.on('message', msg => {
-  if (msg.content === 'l!eğlence') {
-    msg.reply('```l!yaz Yazılarınızı Bot'a Yazdırır.```
-```l!emojiyazi Yazılarınızı Emoji Olarak Yazdırır.```');
-  }    
-
-client.on('message', msg => {
-  if (msg.content === 'l!kullanıcı') {
-    msg.reply('```l!botbilgi Botun Bilgilerini Gösterir.```
-```l!botöneri Botumuza Eklenmesini Istediğini Bu Komutla Ulaştırabilirsiniz!```');
-  }    
-    
-});
-
   
 let prefix = ayar.prefix;
 
