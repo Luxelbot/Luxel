978f5b1957d2f2de9bef7d5433d336d242cfd618
Luxelbot <49617463+Luxelbot@users.noreply.github.com>
2019-04-21 22:33:14 +0300

    Update otorol.js

--- komutlar/otorol.js
+++ komutlar/otorol.js
@@ -1,6 +1,5 @@
 exports.run = async (bot, message, args) =>
 {
-  	let profil = JSON.parse(fs.readFileSync("./autorole.json", "utf8"));
   var mentionedChannel = message.mentions.channels.first();
   if (!mentionedChannel && args[0] !== "sıfırla") return message.channel.send("Ayarlamam İçin Bir Rol Etiketlemelisin. \nRolü Etiketleyemiyorsan **Rolün Etiketleme Seçeneğini Aktif Etmeyi Unutma** \nÖrnek Kullanım : /otorol @rol #kanal \nÖnemli Not!!: Oto Rol Vermem İçin Verilecek Rolün Üstünde Bir Rolüm Olmalı Yoksa Rolü Veremem :)");
   if (message.guild.member(message.author.id).hasPermission(0x8))
@@ -22,7 +21,6 @@ exports.run = async (bot, message, args) =>
 	profil[message.guild.id].sayi = mentionedRole.id
   profil[message.guild.id].kanal = mentionedChannel.id
 	
-	fs.writeFile("./autorole.json", JSON.stringify(profil), (err) => {
 		console.log(err)
 
 	})
