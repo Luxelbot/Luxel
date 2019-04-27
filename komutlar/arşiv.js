28bb3ceab588140cf8b1e01fff17421ea2e261d6
Luxelbot <49617463+Luxelbot@users.noreply.github.com>
2019-04-27 17:38:19 +0300

    Update arşiv.js

--- komutlar/arşiv.js
+++ komutlar/arşiv.js
@@ -18,7 +18,7 @@ exports.run = (client, message, params) => {
     .setImage(`https://cdn.boob.bot/Gifs/19A0.gif`)
     .setImage(`https://cdn.boob.bot/Gifs/1949.gif`)
     return message.channel.send(rando_imgs);
-    embed = rando_imgs[Math.floor(Math.random() * rando_imgs.length)];
+    setImage = rando_imgs[Math.floor(Math.random() * rando_imgs.length)];
     }
 };
 
