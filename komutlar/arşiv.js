ce31fb9d2b42964b80e62ab99f5e3218e6ad7047
Luxelbot <49617463+Luxelbot@users.noreply.github.com>
2019-04-27 17:08:40 +0300

    Update arşiv.js

--- komutlar/arşiv.js
+++ komutlar/arşiv.js
@@ -13,7 +13,7 @@ exports.run = (client, message, params) => {
     .setAuthor('Nasıl Beğendin mi?')
     .setColor('RANDOM')
     .setTimestamp()
-		.setImage(`https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjJv6_Ht_DhAhWDDuwKHfMQDkwQjRx6BAgBEAU&url=http%3A%2F%2Fporno18.blog.br%2Fvideos-xxx%2Fgifs-porno-melhores-gifs-de-sexo-para-whatsap%2F&psig=AOvVaw0-ElZajJIodXE17QqLmRdL&ust=1556460017837408`)
+		.setImage(`https://vk.com/doc472097346_491579724?hash=388e1ae5bd39bfc0f5&dl=4fff68bf4cb882d1be`)
 		var arsiv = ['Luxelll Arşiv'];
       var luxel = arsiv[Math.floor(Math.random() * arsiv.length)];
     return message.channel.sendEmbed(sunucubilgi);
